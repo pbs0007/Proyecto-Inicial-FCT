b5f315627352361c01176c61549defde7d5f1699
ncf0013 <ncf0013@alu.medac.es>
2025-03-24 16:04:27 +0100

    Creación carpeta components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { createClient } from '@supabase/supabase-js'
 import './App.css'
 
 const supabase = createClient(import.meta.env.VITE_PROJECT_URL, import.meta.env.VITE_APIKEY)
 console.log(supabase)
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
