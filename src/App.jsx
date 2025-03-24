e5ea8cc5ad60d10fd8228ce41adc165d10ecf036
Pedro Barrera <pbs0007@alu.medac.es>
2025-03-24 19:33:41 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-import { createClient } from '@supabase/supabase-js'
-import './App.css'
+import { useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+import "./App.css";
+import HeaderComponent from "./components/HeaderComponent";
+import SidebarComponent from "./components/SidebarComponent";
 
-const supabase = createClient(import.meta.env.VITE_PROJECT_URL, import.meta.env.VITE_APIKEY)
-console.log(supabase)
+const supabase = createClient(
+  import.meta.env.VITE_PROJECT_URL,
+  import.meta.env.VITE_APIKEY
+);
+console.log(supabase);
 function App() {
-
-
   return (
     <>
       <div>
