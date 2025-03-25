9cc985df42293a5363c598f3f571c6f9d25e109e
ncf0013 <ncf0013@alu.medac.es>
2025-03-25 18:07:45 +0100

    Implementado inicio de sesión (autenticación supabase)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
-import { useState } from "react";
-import { createClient } from "@supabase/supabase-js";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import HeaderComponent from "./components/HeaderComponent";
 import SidebarComponent from "./components/SidebarComponent";
+import { supabase } from './supabase/supabaseClient'
+import Auth from './components/Auth'
 
-const supabase = createClient(
-  import.meta.env.VITE_PROJECT_URL,
-  import.meta.env.VITE_APIKEY
-);
-console.log(supabase);
+
 function App() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    // Obtener la sesión actual al cargar la app
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    // Escuchar cambios en el estado de sesión (login, logout, etc.)
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    //Limpia el listener al cerrar app
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
+
+  //Si no hay sesión activa, redirige a la página de inicio de sesiónv(componente Auth)
+  if (!session) {
+    return <Auth></Auth>;
+  }
+
+  //Hay sesión activa, muestra contenido
   return (
     <>
       <div>
--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,25 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { supabase } from "../supabase/supabaseClient";
+
+
 
 //Formulario de inicio de sesión
 function Auth() {
+
+    //Estado para almacenar el email introducido por usuario
+    const [email, setEmail] = useState("")
+
+    //Estado para almacenar la contraseña introducida por usuario
+    const [password, setPassword] = useState("")
+
+    //Estado para mostrar mensaje error si login falla
+    const [error, setError] = useState(null)
+
+    /**Función handleLogin que se ejecuta cuando el usuario envía el formulario 
+     * Llama a supabase para intentar iniciar sesión con email y contraseña
+     */
+    const handleLogin = async (e) => {
+        e.preventDefault()                  //Evita que página recargue al enviar formulario
+        setError(null)                      //Borra errores anteriores
+
+        //Llamada a supabase para iniciar sesión
+        const { error } = await supabase.auth.signInWithPassword({ email, password })
+
+        //Hay error, muestra mensaje
+        if (error) {
+            setError(error.message)
+        } else {
+            //sesión se actualiza automaticamente en el componente App por listener
+        }
+    }
+
     return (
         <div className="auth-container">
             <div className="auth-box">
-                <h2 className="auth-title">
-                    Iniciar sesión
+                <h1 className="auth-title">
+                    Gestión de inventario
+                </h1>
+                <h2 className="auth-subtitle">
+                    Inicio de sesión
                 </h2>
-                <p className="auth-subtitle">
+                <p className="auth-text">
                     Formulario de inicio de sesión
                 </p>
 
-                <form className="auth-form">
+                <form className="auth-form" onSubmit={handleLogin}> {/*form ejecuta handleLogin al enviar*/}
                     <label htmlFor="email">Correo electrónico:  </label>
                     <input
                         type="email"
                         id="email"
                         name="email"
                         placeholder="Introduzca su correo electrónico"
-                        className="auth-input" 
+                        className="auth-input"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                     <br />
 
@@ -30,6 +66,8 @@ function Auth() {
                         name="password"
                         placeholder="Introduzca su contraseña"
                         className="auth-input"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                     <br />
 
@@ -38,6 +76,9 @@ function Auth() {
                     </button>
 
                 </form>
+                {/*Si hay error, muestra mensaje*/}
+                {error && <p className="auth-error">{error}</p>}
+
 
                 <p className="auth-forgot">
                     ¿Ha olvidado su contraseña?
