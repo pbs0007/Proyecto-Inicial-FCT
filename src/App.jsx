e352fa516539d255f4132a1505de1aa20ccd7d45
pbs0007 <pbs0007@alu.medac.es>
2025-03-25 16:30:00 +0100

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import "./App.css";
-import ControlPanelPage from "./pages/ControlPanelPage";
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import ControlPanelPage from "./pages/ControlPanelPage";
+import InventoryPage from "./pages/InventoryPage";
+import ProductForm from "./pages/ProductFormPage";
+import Movememets from "./pages/MovementsPage";
 
 const supabase = createClient(
   import.meta.env.VITE_PROJECT_URL,
@@ -15,6 +18,9 @@ function App() {
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/control-panel" element={<ControlPanelPage />} />
+      <Route path="/inventory" element={<InventoryPage />} />
+      <Route path="/producto-form" element={<ProductForm />} />
+      <Route path="/movements" element={<Movememets />} />
     </Routes>
   );
 }
