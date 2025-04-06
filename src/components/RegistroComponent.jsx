faf1c3bee1bab7a0789e54ece76a25aafebfd19f
mdd0025 <mdd0025@alu.medac.es>
2025-04-07 00:46:39 +0200

    Formulario
    Componente de Link de registro a la página de formulario con su css, componente formulario e inclusión en su página correspondiente

---
+++ src/components/RegistroComponent.jsx
@@ -0,0 +1,23 @@
+import "./RegistroComponente.css";
+import { useNavigate } from 'react-router-dom';
+
+function RegistroComponent(){
+
+    const navigate = useNavigate();
+
+    const handleRedirect = () => {
+      navigate("/producto-form")
+    };
+  
+    return (
+     <div className="Registro">
+        <div className="Componentes">
+             <h2 className="textoRecuadro">¿Desea registrar un nuevo producto?</h2> 
+             <button className="Botonregistro" onClick={handleRedirect}>REGISTRAR PRODUCTO</button>
+        </div>
+     </div>
+    );
+  };
+  
+
+export default RegistroComponent;
