d6d51fde6151996c8866756dcbb09556b44c43c8
ncf0013 <ncf0013@alu.medac.es>
2025-04-07 15:51:15 +0200

    Añado nombre usuario (correo sin dominio) al saludo del menú desplegable

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { LogOut, User } from "lucide-react";
 import { supabase } from "../supabase/supabaseClient";
 import LogoutButton from "./LogoutButton";
@@ -9,6 +9,21 @@ import "./UserMenu.css";
 
 function UserMenu() {
     const [isOpen, setIsOpen] = useState(false);
+
+    const [username, setUsername] = useState("");
+
+    // Obtener el usuario actual 
+    useEffect(() => {
+        const getUserInfo = async () => {
+            const { data, error } = await supabase.auth.getUser();
+            if (data?.user?.email) {
+                const name = data.user.email.split("@")[0];
+                setUsername(name);
+            }
+        };
+
+        getUserInfo();
+    }, []);
 
     const toggleMenu = () => setIsOpen(!isOpen);
 
@@ -25,8 +40,8 @@ function UserMenu() {
 
             {isOpen && (
                 <div className="menu-dropdown">
-                    <p>¡Hola!</p>
-                    
+                    <p className="user-greeting">¡Hola, {username}! </p>
+
                     <div className="user-avatar-wrapper">
                         <User size={80} className="user-avatar" />
                     </div>
